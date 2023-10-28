db383319dbd22c019c163140c5fbedd398156ed4
owl-from-hogvarts <transftank@mail.ru>
2023-10-28 23:18:34 +0300

    chore: update exports

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-export * from "./src/register/register";
+export * as register from "./src/register/register";
 export * as operationBase from "./src/base";
 export { multiplyWithCorrection } from "./src/operations/multiplyWithCorrection";
 export { multiplyBute } from "./src/operations/multiplyBute";
+export { multiplyFast2, multiplyFast4 } from "./src/operations/multiplyFast";
 export { divide } from "./src/operations/division";
 export * as byte from "./src/byte";
