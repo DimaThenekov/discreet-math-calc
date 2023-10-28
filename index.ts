6477164f0a5713fb612bcd055815233653c41b82
owl-from-hogvarts <transftank@mail.ru>
2023-10-28 17:27:03 +0300

    chore: update paths in exports

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from "./src/register/register";
+export * as operationBase from "./src/base";
+export { multiplyWithCorrection } from "./src/operations/multiplyWithCorrection";
+export { multiplyBute } from "./src/operations/multiplyBute";
+export { divide } from "./src/operations/division";
+export * as byte from "./src/byte";
