5b957077252492b8430611889d2c07b4ff7d1371
owl-from-hogvarts <transftank@mail.ru>
2023-11-04 15:03:12 +0300

    chore: generate `.d.ts` files

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,20 @@
-
 import { defineConfig } from "rollup";
-import typescript from "@rollup/plugin-typescript"
+import typescript from "@rollup/plugin-typescript";
+import { dts } from "rollup-plugin-dts";
 
-export default defineConfig({
-  input: "./index.ts",
-  output: {
-    dir: "dist",
-    format: "es",
-    sourcemap: "inline"
+export default defineConfig([
+  {
+    input: "./index.ts",
+    output: {
+      dir: "dist",
+      format: "es",
+      sourcemap: "inline",
+    },
+    plugins: [typescript()],
   },
-  plugins: [
-    typescript()
-  ]
-})
-
+  {
+    input: "./index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+]);
